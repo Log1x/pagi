ad239a3c8e90b87bbd677a5bcf8b66f7c3690090
Brandon <brandon@tendency.me>
2021-03-04 16:49:05 -0600

    chore(lint): add space between bang

--- src/Pagi.php
+++ src/Pagi.php
@@ -66,7 +66,7 @@ class Pagi
             return;
         }
 
-        if (!is_search()) {
+        if (! is_search()) {
             $this->query->put('post_type', get_post_type());
 
             if (is_tax()) {
